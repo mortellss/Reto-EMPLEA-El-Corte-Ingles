24f5f06241115409f3b5c80faf78841c095ac8be
Marta Carrion <mcarber@teleco.upv.es>
2026-08-27 20:06:33 +0200

    Conectar backend con Aiven

--- interfaz/backend/db.py
+++ interfaz/backend/db.py
@@ -1,5 +1,28 @@
+#from sqlalchemy import create_engine
+
+#engine = create_engine(
+#    "mysql+pymysql://root:emplea@localhost/emplea"
+#)
+
+import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_SSL_CA = os.getenv("DB_SSL_CA")
 
 engine = create_engine(
-    "mysql+pymysql://root:emplea@localhost/emplea"
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+    connect_args={
+        "ssl": {
+            "ca": DB_SSL_CA
+        }
+    },
+    pool_pre_ping=True
 )
