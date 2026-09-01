66cfa16fcb7cb060716a87dc62bcd46079d27f00
Marta Carrion <mcarber@teleco.upv.es>
2026-09-01 16:36:55 +0200

    A ver si ahora funciona la generación de planificación

--- FASE_3_calendarizacion/calPRUEBA.py
+++ FASE_3_calendarizacion/calPRUEBA.py
@@ -819,8 +819,12 @@ def crear_calendario_base(trabajadores, calendario, calendario_trabajadores, tar
     solver = cp_model.CpSolver()
 
     # Limitamos el tiempo
-    solver.parameters.max_time_in_seconds = 120
-    solver.parameters.num_search_workers = os.cpu_count() or 8
+    #solver.parameters.max_time_in_seconds = 120
+    #solver.parameters.num_search_workers = os.cpu_count() or 8
+
+    solver.parameters.max_time_in_seconds = 60
+    solver.parameters.num_search_workers = 2
+
     solver.parameters.log_search_progress = True
 
     estado = solver.Solve(modelo)
