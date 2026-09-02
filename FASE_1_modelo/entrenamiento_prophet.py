716fcfe01e74a565b4ade131e972e848b058d232
mortellss <marinaortellssicilia@gmail.com>
2026-09-02 10:51:11 +0200

    a ver si este era el error

--- FASE_1_modelo/entrenamiento_prophet.py
+++ FASE_1_modelo/entrenamiento_prophet.py
@@ -306,7 +306,7 @@ print(
 print(
     f"Periodo visible en interfaz: {fecha_inicio_prediccion.date()} -> {fecha_fin_prediccion.date()}"
 )
-
+'''
 trimestre = forecast[
     ["ds", "yhat", "yhat_lower", "yhat_upper"]
 ].reset_index(drop=True)
@@ -315,7 +315,7 @@ trimestre = forecast[
 trimestre = periodo_mostrado[
     ["ds", "yhat", "yhat_lower", "yhat_upper"]
 ].reset_index(drop=True)
-'''
+
 
 trimestre = trimestre.drop_duplicates(subset=['ds'], keep='last').sort_values('ds').reset_index(drop=True)
 
